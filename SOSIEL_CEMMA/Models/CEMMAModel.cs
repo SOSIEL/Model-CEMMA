403090600ae53499e7e55373b45841fece791b4a
Maxim Davidovsky <m.davidovsky@gmail.com>
2019-09-24 10:18:28 +0300

    Add files via upload

--- SOSIEL_CEMMA/Models/CEMMAModel.cs
+++ SOSIEL_CEMMA/Models/CEMMAModel.cs
@@ -7,31 +7,35 @@ namespace SOSIEL_CEMMA.Configuration
     /// </summary>
     public class CEMMAModel
     {
-        [JsonRequired]
-        public static double Disturbance { get; set; } = 1; // Disturbance size
+        [JsonProperty(Required = Required.Always)]
+        public double Disturbance { get; set; } // Disturbance size
 
-        [JsonRequired]
-        public static double DisturbanceIncrement { get; set; } = 0.5; // Disturbance increment
+        [JsonProperty(Required = Required.Always)]
+        public double DisturbanceIncrement { get; set; } // Disturbance increment
 
-        [JsonRequired]
-        public static double Endowment { get; set; } = 10; // Fixed number of extracted resource units
+        [JsonProperty(Required = Required.Always)]
+        public double Endowment { get; set; } // Fixed number of extracted resource units
 
-        [JsonRequired]
-        public static double Externalities { get; set; } = 4;// Marginal benefit from sharing
+        [JsonProperty(Required = Required.Always)]
+        public double Externalities { get; set; } // Marginal benefit from sharing
 
-        //[JsonRequired]
-        //public static int NumOfIterations { get; set; } //= 100;  Number of iterations.
+        [JsonProperty(Required = Required.Always)]
+        public double PropOfContrib { get; set; } // Proportion of agents who share all
 
-        //[JsonRequired]
-        //public static int NumOfAgents { get; set; } //= 100;  The number of agents in a simulation. Must be equal to or greater than ten.
+        [JsonProperty(Required = Required.Always)]
+        public double VacantSpots { get; set; } // Proportion of empty spots
 
-        [JsonRequired]
-        public static double PropOfContrib { get; set; } = 0.30; // Proportion of agents who share all
+        [JsonProperty(Required = Required.Always)]
+        public double ResourceRegenerationRate { get; set; } // The rate at which the resource regenerates
 
-        [JsonRequired]
-        public static double VacantSpots { get; set; } = 0.3; // Proportion of empty spots
+        [JsonProperty(Required = Required.Always)]
+        public double PopulationGrowthRate { get; set; } // The rate at which the population grows each iteration
 
-        //[JsonRequired]
-        //public static int SocialSpaceSize { get; set; } = (int)System.Math.Round(NumOfAgents / (1 - VacantSpots), 0); // The number of rows and columns in the SocialSpace (See Subsec. 2.1). When necessary, round up to the nearest integer.
+        [JsonProperty(Required = Required.Always)]
+        public double BaseResourceUnits { get; set; } // Base resource unit requirement - the amount of resource units an agent requires during an iteration in order to survive
+
+        [JsonProperty(Required = Required.Always)]
+        public double ExtractionDifficulty { get; set; } // Essentially the difficulty in extracting should increase as the amount of resource decreases. By increase in difficulty is meant that for every desired unit, less and less is actually extracted.
+
     }
 }
